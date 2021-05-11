d211504055ed7fb3784e500c3463ea037f12c7fe
Xuhan Liu <xuhanliu@qq.com>
2021-05-11 20:16:34 +0200

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -127,4 +127,4 @@ def A2AR(input, out):
 if __name__ == '__main__':
     // ZINC('zinc/', 'data/ZINC.txt')
     corpus('data/zinc.txt', 'data/zinc')
-    A2AR('data/A2AR_raw.txt', 'data/CHEMBL251.txt')
+    A2AR('data/A2AR_raw.txt', 'data/chembl251_corpus.txt')
